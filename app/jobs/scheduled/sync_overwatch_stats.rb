faf4b60c5e92f942c0db81edd066706bfa81e7da
Daniel Marquard <daniel@netidy.com>
2016-12-07 20:08:29 -0500

    Reverted interval to daily

--- app/jobs/scheduled/sync_overwatch_stats.rb
+++ app/jobs/scheduled/sync_overwatch_stats.rb
@@ -1,6 +1,6 @@
 module Jobs
   class SyncOverwatchStats < Jobs::Scheduled
-    every 1.hour
+    every 1.day
 
     def execute(args)
       ::OverwatchStatsServices::MasterOverwatch.sync if SiteSetting.discourse_overwatch_stats_enabled
